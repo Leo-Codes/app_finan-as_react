9b830ab63d7da21790926446f5081c4b0465c64a
Leocodes <leo.ccodes@gmail.com>
2023-02-13 22:20:09 -0300

    initial commit

---
+++ src/components/Resume/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 95vw;
+    width: 80%;
+    margin: 0 auto;
+    display:flex;
+    /* flex-direction:row; */
+    gap: 15px;
+    margin-top: -50px;
+    justify-content:space-around;
+
+    font-family:'Poppins';
+
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    *{
+        margin:0;
+        padding:0;
+
+    }
+    body{
+        background: #e3e1e3;
+        font-family: "Poppins"
+    }
+`;
+
+export default GlobalStyle
+
+
